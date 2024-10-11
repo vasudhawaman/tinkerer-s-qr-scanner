07a1240e956145310966c9289737ff6990ee83f9
Gurupranav-tech <67361835+Gurupranav-tech@users.noreply.github.com>
2024-10-11 14:46:10 +0530

    Added react router for navigation and tailwindcss for styling and setup a barebone boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import QrScanner from "./pages/qrcode_scanner";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/qrscan",
+    element: <QrScanner />,
+  },
+]);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
-
-export default App;
---
+++ frontend/src/pages/qrcode_scanner.js
@@ -0,0 +1,3 @@
+export default function QrScanner() {
+  return <h1 className="text-xl text-blue-500">QR Code Scanner</h1>;
+}
